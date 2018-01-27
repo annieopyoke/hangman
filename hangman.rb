40fabb207893b338029bb08693c34a6d2a00e7e8
Annie Opyoke <annieopyoke@gmail.com>
2018-01-27 14:09:11 -0500

    first commit

---
+++ hangman.rb
@@ -0,0 +1,6 @@
+def word_setup(secert_word)
+secert_word.length
+end
+
+def word_blanks(secert_word)
+end
---
+++ hangmantest.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require_relative "hangman.rb"
+class Testhangman < Minitest::Test
+
+def test_assert_that_1_is_1
+	assert_equal(1, 1)
+end
+
+def test_assert_that_dog_length_is_3
+	secert_word = "dog"
+	assert_equal(3,word_setup(secert_word))
+end
+
+def test_assert_that_dog_is_three_underscores
+	secert_word = "dog"
+	assert_equal(["_", "_", "_"},word_blanks(secert_word))
+end
