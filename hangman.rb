c8a56a8f234d4477b86754537c8c1375f411d19d
Annie Opyoke <annieopyoke@gmail.com>
2018-01-28 10:24:47 -0500

    added test for f

--- hangman.rb
+++ hangman.rb
@@ -14,16 +14,21 @@ def guess_letter(secert_word, guess)
 
 	secert_word = secert_word.split("")
 	updated_array = word_blanks(secert_word)
-if guess == secert_word[0]
-	updated_array[0] = guess
+	array_counter = 0
+	word_length = word_setup(secert_word)
+until array_counter == word_length
+if guess == secert_word[array_counter]
+	updated_array[array_counter] = guess
 	
 end
-if guess == secert_word[1]
-	updated_array[1] = guess
+
+array_counter += 1
+# if guess == secert_word[1]
+# 	updated_array[1] = guess
 	
-end
-if guess == secert_word[2]
-	updated_array[2] = guess
+# end
+# if guess == secert_word[2]
+# 	updated_array[2] = guess
 	
 end
 
